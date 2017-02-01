0d446650bb815dc9da887a42d71e1bf536f8b283
mujichOk <alexeymalyarov@gmail.com>
2017-02-01 21:50:59 +0200

    default in-memory provider, default callbacks

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const util = require('util');
 
 module.exports = NodeRateLimiter;
 
+NodeRateLimiter.defaultRateLimit = 5000;
+NodeRateLimiter.defaultExpiration = 1000 * 60 * 60;
+NodeRateLimiter.defaultTimeout = 500;
+
 NodeRateLimiter.TimeoutError = TimeoutError;
 
 util.inherits(TimeoutError, Error);
@@ -10,15 +14,15 @@ util.inherits(TimeoutError, Error);
 function NodeRateLimiter(opts) {
     opts = opts || {};
     
-    const adaptor = opts.adaptor;
+    const adaptor = opts.adaptor || new InMemoryAdaptor();
     if (!adaptor) {
         throw new Error('adaptor not defined');
     }
 
-    let isPrepared = false;
+    let isPrepared = typeof adaptor.prepare !== 'function';
 
-    this.reset =(id, callback) => prepare(callback, () => adaptor.reset(id, callback));
-    this.get = (id, opts, callback) => prepare(callback, () => adaptor.get(id, opts, callback));
+    this.reset =(id, callback) => (callback = callback || foo) && prepare(callback, () => adaptor.reset(id, callback));
+    this.get = (id, opts, callback) => (callback = callback || foo) && prepare(callback, () => adaptor.get(id, opts, callback));
 
 
     function prepare(fail, next) {
@@ -44,3 +48,48 @@ function TimeoutError(message, extra) {
     this.message = message;
     this.extra = extra;
 }
+
+
+function InMemoryAdaptor() {
+    const map = {};
+
+    this.reset = (id, callback) => { 
+        delete map[id];
+        callback();
+    };
+
+    this.get = (id, opts, callback) => {
+        const meta = map[id];
+        const date = +new Date();
+
+        if (meta && meta.expire <= date) {
+            meta = null;
+            delete map[id];
+        }
+
+        if (!meta) {
+            meta = map[id] = {
+                total: 0,
+                limit: opts && opts.limit || NodeRateLimiter.defaultRateLimit,
+                expire: date + (opts && opts.expire || NodeRateLimiter.defaultExpiration)
+            };
+        }
+
+        meta.total++;
+
+        if (meta.total >= meta.limit) {
+            meta.total = meta.limit;
+            
+            const result = {
+                limit: meta.limit,
+                remaining: meta.limit - meta.total,
+                refresh: meta.expire - date
+            };
+            return callback(null, result);
+        }
+
+        return callback(null);
+    };
+}
+
+function foo() {}
