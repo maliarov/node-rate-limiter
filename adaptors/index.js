fa38d385f70a21cd4fd389dc15ec2b7c7d5b54d9
Oleksii Maliarov <lex@debitoor.com>
2017-02-01 11:03:15 +0200

    remove inbuild redis, add timeout error class, change init process

--- adaptors/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    RedisAdaptor: require('./redis/adaptor')
-};
--- index.js
+++ index.js
@@ -1,23 +1,70 @@
-module.exports = function (adaptor) {
-    let isPrepared = false;
+const util = require('util');
+const once = require('once');
 
-    this.reset = (id, callback) => prepare(callback, () => adaptor.reset(id, callback));
+const adaptors = {};
+
+module.exports = NodeRateLimiter;
+
+NodeRateLimiter.registerAdaptor = registerAdaptor;
+NodeRateLimiter.TimeoutError = TimeoutError;
+
+util.inherits(TimeoutError, Error);
+
+
+function NodeRateLimiter(opts) {
+    opts = opts || {};
+
+    let adaptor = opts.adaptor;
+
+    if (!adaptor && opts.adaptorName) {
+        const Adaptor = adaptors[opts.adaptorName] && adaptors[opts.adaptorName].versions[opts.adaptorVer || 'default'];
+        if (!Adaptor) {
+            throw new Error('adaptor not initialized');
+        }
+
+        adaptor = new Adaptor(opts.adaptorOpts);
+    }
+
+    opts.adaptorName = adaptor.name;
+    opts.adaptorVer = adaptor.ver;
+
+    this.reset =(id, callback) => prepare(callback, () => adaptor.reset(id, callback));
     this.get = (id, opts, callback) => prepare(callback, () => adaptor.get(id, opts, callback));
 
 
-    function prepare(fail, callback) {
+    function prepare(fail, next) {
         if (isPrepared) {
-            return callback();
+            return next();
         }
 
         adaptor.prepare((err) => {
             if (err) {
                 return fail(err);
             }
+            
             isPrepared = true;
-            callback();
+            next();
         });
     }
+}
+
+function TimeoutError(message, extra) {
+    Error.captureStackTrace(this, this.constructor);
+
+    this.name = this.constructor.name;
+    this.message = message;
+    this.extra = extra;
 };
 
-module.exports.adaptors = require('./adaptors');
+function registerAdaptor(opts) {
+    const ver = opts.ver || 'default';
+
+    adaptors[opts.name] = adaptors[opts.name] || {};
+    adaptors[opts.name].ver = adaptors[opts.name].ver || {};
+
+    if (adaptors[opts.name].versions[ver]) {
+        throw new Error(`adaptor with same name [${opts.name}] and version [${ver}] already registered`);
+    }
+
+    adaptors[opts.name].versions[ver] = opts;
+}
